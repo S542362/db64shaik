6a28c48de31dc4f7f2fadf91212c3c62b0c49820
S542362 <77766701+S542362@users.noreply.github.com>
2021-04-09 18:22:38 -0500

    updated screen shot

--- controllers/horse.js
+++ controllers/horse.js
@@ -14,9 +14,18 @@ exports.horse_list = async function(req, res) {
         };
 
 // for a specific horse.
-exports.horse_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: horse detail: ' + req.params.id);
+// for a specific Costume.
+exports.horse_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await horse.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
 // Handle horse create on POST.
 exports.horse_create_post = async function(req, res) {
     console.log(req.body)
@@ -44,9 +53,24 @@ exports.horse_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: horse delete DELETE ' + req.params.id);
 };
 // Handle horse update form on PUT.
-exports.horse_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: horse update PUT' + req.params.id);
+exports.horse_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await horse.findById(req.params.id)
+        // Do updates of properties
+        if(req.body.teamname) toUpdate.teamname = req.body.teamname;
+        if(req.body.country) toUpdate.country = req.body.country;
+        if(req.body.number_of_players) toUpdate.number_of_players = req.body.number_of_players;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
+
 // VIEWS
 // Handle a show all view
 exports.horse_view_all_Page = async function(req, res) {
